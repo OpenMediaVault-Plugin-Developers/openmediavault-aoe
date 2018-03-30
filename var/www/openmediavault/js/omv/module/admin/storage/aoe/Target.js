d13b5b003028cd661b8113945a7ef012d4fd35b7
Aaron Murray <plugins@omv-extras.org>
2018-03-30 17:55:18 -0500

    initial commit

---
+++ var/www/openmediavault/js/omv/module/admin/storage/aoe/Target.js
@@ -0,0 +1,125 @@
+/**
+ * Copyright (C) 2014-2018 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/window/Form.js")
+// require("js/omv/workspace/window/plugin/ConfigObject.js")
+// require("js/omv/form/field/plugin/FieldInfo.js")
+
+Ext.define('OMV.module.admin.storage.aoe.Target', {
+    extend: 'OMV.workspace.window.Form',
+    requires: [
+        'OMV.form.field.plugin.FieldInfo',
+        'OMV.workspace.window.plugin.ConfigObject'
+    ],
+
+    plugins: [{
+        ptype: 'configobject'
+    }],
+
+    hideResetButton: true,
+
+    rpcService: 'aoe',
+    rpcGetMethod: 'getTarget',
+    rpcSetMethod: 'setTarget',
+
+    getFormItems: function() {
+        return [{
+            xtype: 'textfield',
+            name: 'shelf',
+            fieldLabel: _('Shelf'),
+            allowBlank: false,
+            plugins: [{
+                ptype: 'fieldinfo',
+                text: _('This should be the shelf address (major AoE address) of the AoE device to create.')
+            }]
+        },{
+            xtype: 'textfield',
+            name: 'slot',
+            fieldLabel: _('Slot'),
+            value: '',
+            allowBlank: false,
+            plugins: [{
+                ptype: 'fieldinfo',
+                text: _('This should be the slot address (minor AoE address) of the AoE device to create.')
+            }]
+        },{
+            xtype: "combo",
+            name: "netif",
+            fieldLabel: _("NIC"),
+            emptyText: _("Select a device ..."),
+            queryMode: "local",
+            store: Ext.create("OMV.data.Store", {
+                autoLoad: true,
+                model: OMV.data.Model.createImplicit({
+                    idProperty: "devicename",
+                    fields: [
+                        { name: "devicename", type: "string" }
+                    ]
+                }),
+                proxy: {
+                    type: "rpc",
+                    rpcData: {
+                        service: "Network",
+                        method: "getEthernetCandidates"
+                    }
+                },
+                sorters: [{
+                    direction: "ASC",
+                    property: "devicename"
+                }]
+            }),
+            displayField: "devicename",
+            valueField: "devicename",
+            allowBlank: false,
+            forceSelection: true,
+            triggerAction: "all",
+            plugins: [{
+                ptype: 'fieldinfo',
+                text: _('The name of the ethernet network interface to use for AoE communications.')
+            }]
+        },{
+            xtype: 'textfield',
+            name: 'filename',
+            fieldLabel: _('Filename'),
+            value: '',
+            allowBlank: false,
+            plugins: [{
+                ptype: 'fieldinfo',
+                text: _('The name of the regular file or block device to export.')
+            }]
+        },{
+            xtype: 'checkbox',
+            name: 'direct',
+            fieldLabel: _('Direct'),
+            checked: false,
+            plugins: [{
+                ptype: 'fieldinfo',
+                text: _('Selects O_DIRECT mode for accessing the underlying block device.')
+            }]
+        },{
+            xtype: 'checkbox',
+            name: 'sync',
+            fieldLabel: _('Sync'),
+            checked: false,
+            plugins: [{
+                ptype: 'fieldinfo',
+                text: _('Selects O_SYNC mode for accessing the underlying block device, so all writes are committed to disk before returning to the client.')
+            }]
+        }];
+    }
+});
---
+++ var/www/openmediavault/js/omv/module/admin/storage/aoe/Targets.js
@@ -0,0 +1,147 @@
+/**
+ * Copyright (C) 2014-2018 OpenMediaVault Plugin Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// require("js/omv/WorkspaceManager.js")
+// require("js/omv/workspace/grid/Panel.js")
+// require("js/omv/data/Store.js")
+// require("js/omv/data/Model.js")
+// require("js/omv/data/proxy/Rpc.js")
+// require("js/omv/module/admin/storage/aoe/Target.js")
+
+Ext.define('OMV.module.admin.storage.aoe.Targets', {
+    extend: 'OMV.workspace.grid.Panel',
+    requires: [
+        'OMV.data.Store',
+        'OMV.data.Model',
+        'OMV.data.proxy.Rpc',
+        'OMV.module.admin.storage.aoe.Target'
+    ],
+
+    hidePagingToolbar: false,
+    reloadOnActivate: true,
+
+    columns: [{
+        xtype: "textcolumn",
+        header: _('UUID'),
+        hidden: true,
+        dataIndex: 'uuid'
+    },{
+        xtype: "textcolumn",
+        header: _('Shelf'),
+        flex: 1,
+        sortable: true,
+        dataIndex: 'shelf'
+    },{
+        xtype: "textcolumn",
+        header: _('Slot'),
+        flex: 1,
+        sortable: true,
+        dataIndex: 'slot'
+    },{
+        xtype: "textcolumn",
+        header: _('NIC'),
+        flex: 1,
+        sortable: true,
+        dataIndex: 'netif'
+    },{
+        xtype: "textcolumn",
+        header: _('Filename'),
+        flex: 1,
+        sortable: true,
+        dataIndex: 'filename'
+    }],
+
+    store: Ext.create('OMV.data.Store', {
+        autoLoad: true,
+        model: OMV.data.Model.createImplicit({
+            idProperty: 'uuid',
+            fields: [{
+                name: 'uuid',
+                type: 'string'
+            },{
+                name: 'shelf',
+                type: 'integer'
+            },{
+                name: 'slot',
+                type: 'integer'
+            },{
+                name: 'netif',
+                type: 'string'
+            },{
+                name: 'filename',
+                type: 'string'
+            }]
+        }),
+        proxy: {
+            type: 'rpc',
+            rpcData: {
+                'service': 'Aoe',
+                'method': 'getTargetList'
+            }
+        }]
+    }),
+
+    onAddButton: function() {
+        Ext.create('OMV.module.admin.storage.aoe.Target', {
+            title: _('Add target'),
+            uuid: OMV.UUID_UNDEFINED,
+            listeners: {
+                scope: this,
+                submit: function() {
+                    this.doReload();
+                }
+            }
+        }).show();
+    },
+
+    onEditButton: function() {
+        var record = this.getSelected();
+
+        Ext.create('OMV.module.admin.storage.aoe.Target', {
+            title: _('Edit target'),
+            uuid: record.get('uuid'),
+            listeners: {
+                scope: this,
+                submit: function() {
+                    this.doReload();
+                }
+            }
+        }).show();
+    },
+
+    doDeletion: function(record) {
+        OMV.Rpc.request({
+            scope: this,
+            callback: this.onDeletion,
+            rpcData: {
+                service: 'aoe',
+                method: 'delete',
+                params: {
+                    uuid: record.get('uuid')
+                }
+            }
+        });
+    }
+});
+
+OMV.WorkspaceManager.registerPanel({
+    id: 'targets',
+    path: '/storage/aoe',
+    text: _('Targets'),
+    position: 10,
+    className: 'OMV.module.admin.storage.aoe.Targets'
+});
